2c0bb8f4346312ada57c0bced25f0dbdc8ee385b
puneet998 <puneetprajapati9936@gmail.com>
2026-03-02 22:28:12 +0530

    url chnage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import History from './pages/History';
 import Prising from './pages/Prising';
 import PaymentSuccess from './pages/PaymentSuccess';
 import PaymentFailed from './pages/PaymentFailed';
-export const serverUrl ="http://localhost:8000";
+export const serverUrl ="https://notegenius-ai.onrender.com";
 
 const App = () => {
   const dispatch = useDispatch();
